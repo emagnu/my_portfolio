88128f21339aab57b1e5b147bb6f1f221f12beb9
Emmanuele <emagnubcp@gmail.com>
2024-02-25 23:53:25 +0000

    part 5

---
+++ lib/constants/size.dart
@@ -0,0 +1,7 @@
+//   ///
+//  Import LIBRARIES
+//  Import FILES
+//  //   ///
+
+const double kMinDesktopWidth = 600.0;
+const double kMinTabletWidth = 768;
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,9 +1,12 @@
 //   ///
 //  Import LIBRARIES
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 //  Import FILES
 import '../constants/colors.dart';
+import '../constants/size.dart';
 import '../widgets/drawer_mobile.dart';
+import '../widgets/header_desktop.dart';
 import '../widgets/header_mobile.dart';
 //  //   ///
 
@@ -19,59 +22,65 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      key: scaffoldKey,
-      backgroundColor: CustomColor.scaffoldBg,
-      // appBar: AppBar(title: const Text('Home Page')),
-      endDrawer: const DrawerMobile(),
-      body: ListView(
-        scrollDirection: Axis.vertical,
-        children: <Widget>[
-          //  MAIN
-          // const HeaderDesktop(),
-          HeaderMobile(
-            onLogoTap: () {},
-            onMenuTap: () {
-              scaffoldKey.currentState?.openEndDrawer();
-            },
-          ),
-          //  SKILLS
-          Container(
-            height: 500.0,
-            width: double.infinity,
-            color: Colors.blueGrey,
-            child: const Center(
-              child: Text('Skills'),
+    return LayoutBuilder(builder: (context, constraints) {
+      return Scaffold(
+        key: scaffoldKey,
+        backgroundColor: CustomColor.scaffoldBg,
+        // appBar: AppBar(title: const Text('Home Page')),
+        endDrawer: constraints.maxWidth >= kMinDesktopWidth
+            ? null
+            : const DrawerMobile(),
+        body: ListView(
+          scrollDirection: Axis.vertical,
+          children: <Widget>[
+            //  MAIN
+            if (constraints.maxWidth >= kMinDesktopWidth)
+              const HeaderDesktop()
+            else
+              HeaderMobile(
+                onLogoTap: () {},
+                onMenuTap: () {
+                  scaffoldKey.currentState?.openEndDrawer();
+                },
+              ),
+            //  SKILLS
+            Container(
+              height: 500.0,
+              width: double.infinity,
+              color: Colors.blueGrey,
+              child: const Center(
+                child: Text('Skills'),
+              ),
             ),
-          ),
 
-          // PROJECTS
-          Container(
-            height: 500.0,
-            width: double.infinity,
-            color: Colors.blueGrey,
-            child: const Center(
-              child: Text('Projects'),
+            // PROJECTS
+            Container(
+              height: 500.0,
+              width: double.infinity,
+              color: Colors.blueGrey,
+              child: const Center(
+                child: Text('Projects'),
+              ),
             ),
-          ),
 
-          //  CONTACT
-          Container(
-            height: 500.0,
-            width: double.infinity,
-            color: Colors.blueGrey,
-            // child: ,
-          ),
+            //  CONTACT
+            Container(
+              height: 500.0,
+              width: double.infinity,
+              color: Colors.blueGrey,
+              // child: ,
+            ),
 
-          //  FOOTER
-          Container(
-            height: 500.0,
-            width: double.infinity,
-            color: Colors.blueGrey,
-            // child: ,
-          ),
-        ],
-      ),
-    );
+            //  FOOTER
+            Container(
+              height: 500.0,
+              width: double.infinity,
+              color: Colors.blueGrey,
+              // child: ,
+            ),
+          ],
+        ),
+      );
+    });
   }
 }
