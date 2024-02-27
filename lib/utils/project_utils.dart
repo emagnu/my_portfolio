2a520500e1d4f3d3eef82a776efceda3c3a3ec21
Emmanuele <emagnubcp@gmail.com>
2024-02-27 15:31:06 +0000

    part 10

---
+++ lib/utils/project_utils.dart
@@ -0,0 +1,78 @@
+//   ///
+//  Import LIBRARIES
+//  Import FILES
+//  //   ///
+
+class ProjectUtils {
+  final String image;
+  final String title;
+  final String subtitle;
+  final String? androidLink;
+  final String? iosLink;
+  final String? webLink;
+
+  ProjectUtils({
+    required this.image,
+    required this.title,
+    required this.subtitle,
+    this.androidLink,
+    this.iosLink,
+    this.webLink,
+  });
+}
+
+//  ---------------
+/// Hobby Project
+List<ProjectUtils> hobbyProjectUtils = [
+  ProjectUtils(
+    image: 'assets/projects/1.png',
+    title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    subtitle: 'Vivamus pretium tincidunt mauris vel lobortis.',
+    androidLink:
+        'https://play.google.com/store/apps/details?id=kr.co.evolcano.donotstudy',
+    iosLink:
+        'https://apps.apple.com/kr/app/%EC%98%81%EC%96%B4%EB%A8%B8%EB%A6%AC-%EA%B3%B5%EC%9E%91%EC%86%8C/id1507102714',
+  )
+];
+
+//  ---------------
+/// Personal Project
+List<ProjectUtils> personalProjectUtils = [
+  ProjectUtils(
+      image: 'assets/projects/2.png',
+      title: 'Etiam tincidunt ante arcu, sed pharetra orci maximus quis.',
+      subtitle:
+          ' Proin et sagittis est, nec lobortis sapien. Nam mattis mattis odio vel ullamcorpe.',
+      webLink: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+];
+
+//  ---------------
+/// Company Project
+List<ProjectUtils> companyProjectUtils = [
+  ProjectUtils(
+      image: 'assets/projects/3.png',
+      title: 'Morbi sed lorem bibendum, molestie justo nec, tempor sem.',
+      subtitle: 'Pellentesque gravida ullamcorper finibus.',
+      androidLink: '')
+];
+
+//  ---------------
+/// School Project
+List<ProjectUtils> schoolProjectUtils = [
+  ProjectUtils(
+    image: 'assets/projects/4.png',
+    title: 'Fusce laoreet est a mi maximus tincidunt tristique massa.',
+    subtitle: 'Praesent eros elit, sollicitudin eget tortor at. ',
+    webLink: 'https://pub.dev/packages/signals',
+  )
+];
+
+//  ---------------
+/// Other Project
+List<ProjectUtils> otherProjectUtils = [
+  ProjectUtils(
+    image: 'assets/projects/5.png',
+    title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    subtitle: 'Vivamus pretium tincidunt mauris vel lobortis.',
+  ),
+];
---
+++ lib/widgets/project_card_widget.dart
@@ -0,0 +1,105 @@
+//   ///
+//  Import LIBRARIES
+import 'package:flutter/material.dart';
+import 'dart:js' as js;
+//  Import Files
+import '../constants/colors.dart';
+import '../utils/project_utils.dart';
+//  //   ///
+
+class ProjectCardWidget extends StatelessWidget {
+  final ProjectUtils project;
+
+  const ProjectCardWidget({super.key, required this.project});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      clipBehavior: Clip.antiAlias,
+      height: 280.0,
+      width: 250.0,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: CustomColor.bgLight2,
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Image.asset(
+            // Image
+            project.image,
+            height: 140.0,
+            width: 250.0,
+            fit: BoxFit.cover,
+          ),
+          // const SizedBox(height: 10.0),
+          // Title
+          Padding(
+            padding: const EdgeInsets.fromLTRB(12.0, 15.0, 12.0, 12.0),
+            child: Text(
+              project.title,
+              style: const TextStyle(
+                fontWeight: FontWeight.w600,
+                color: CustomColor.whitePrimary,
+              ),
+            ),
+          ),
+          // SubTitle
+          Padding(
+            padding: const EdgeInsets.fromLTRB(12.0, 0.0, 12.0, 12.0),
+            child: Text(
+              project.subtitle,
+              style: const TextStyle(
+                fontSize: 10.0,
+                color: CustomColor.whiteSecondary,
+              ),
+            ),
+          ),
+          // Footer
+          const Spacer(),
+          Container(color: CustomColor.bgLight1, height: 1.0),
+          Padding(
+            padding:
+                const EdgeInsets.symmetric(horizontal: 12.0, vertical: 10.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                const Text(
+                  'Available on:',
+                  style: TextStyle(
+                      fontSize: 10, color: CustomColor.yellowSecondary),
+                ),
+                const Spacer(),
+                if (project.iosLink != null)
+                  InkWell(
+                    onTap: () {
+                      js.context.callMethod('open', [project.iosLink]);
+                    },
+                    child: Image.asset('assets/icons/apple.png', width: 12),
+                  ),
+                if (project.androidLink != null)
+                  Padding(
+                    padding: const EdgeInsets.only(left: 2.0),
+                    child: InkWell(
+                      onTap: () {
+                        js.context.callMethod('open', [project.androidLink]);
+                      },
+                      child: Image.asset('assets/icons/android.png', width: 15),
+                    ),
+                  ),
+                if (project.webLink != null)
+                  InkWell(
+                    onTap: () {
+                      js.context.callMethod('open', [project.webLink]);
+                    },
+                    child: Image.asset('assets/icons/web.png', width: 15),
+                  ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
